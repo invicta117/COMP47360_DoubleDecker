af864d4d5acc13b6be2aae0b8d54ce726851b3e8
KC <cooneykillian@gmail.com>
2021-06-22 20:21:53 +0100

    adding sql file to create weather database and scraper to query api and save data to database

---
+++ SCRAPER/CREATE_WEATHER_DATABASE.sql
@@ -0,0 +1,159 @@
+create database if not exists weather;
+
+CREATE TABLE IF NOT EXISTS weather.current (
+	hr int,
+    dt datetime,
+    temp float,
+    pressure float,
+    humidity float,
+    dewpoint float,
+    rain float
+);
+
+insert into weather.current (hr, dt, temp, pressure, humidity, dewpoint, rain)
+    Values (-1, NULL, NULL, NULL, NULL, NULL, NULL);
+
+insert into weather.current (hr, dt, temp, pressure, humidity, dewpoint, rain)
+    Values (0, NULL, NULL, NULL, NULL, NULL, NULL);
+
+insert into weather.current (hr, dt, temp, pressure, humidity, dewpoint, rain)
+    Values (1, NULL, NULL, NULL, NULL, NULL, NULL);
+
+insert into weather.current (hr, dt, temp, pressure, humidity, dewpoint, rain)
+    Values (2, NULL, NULL, NULL, NULL, NULL, NULL);
+
+insert into weather.current (hr, dt, temp, pressure, humidity, dewpoint, rain)
+    Values (3, NULL, NULL, NULL, NULL, NULL, NULL);
+
+insert into weather.current (hr, dt, temp, pressure, humidity, dewpoint, rain)
+    Values (4, NULL, NULL, NULL, NULL, NULL, NULL);
+
+insert into weather.current (hr, dt, temp, pressure, humidity, dewpoint, rain)
+    Values (5, NULL, NULL, NULL, NULL, NULL, NULL);
+
+insert into weather.current (hr, dt, temp, pressure, humidity, dewpoint, rain)
+    Values (6, NULL, NULL, NULL, NULL, NULL, NULL);
+
+insert into weather.current (hr, dt, temp, pressure, humidity, dewpoint, rain)
+    Values (7, NULL, NULL, NULL, NULL, NULL, NULL);
+
+insert into weather.current (hr, dt, temp, pressure, humidity, dewpoint, rain)
+    Values (8, NULL, NULL, NULL, NULL, NULL, NULL);
+
+insert into weather.current (hr, dt, temp, pressure, humidity, dewpoint, rain)
+    Values (9, NULL, NULL, NULL, NULL, NULL, NULL);
+
+insert into weather.current (hr, dt, temp, pressure, humidity, dewpoint, rain)
+    Values (10, NULL, NULL, NULL, NULL, NULL, NULL);
+
+insert into weather.current (hr, dt, temp, pressure, humidity, dewpoint, rain)
+    Values (11, NULL, NULL, NULL, NULL, NULL, NULL);
+
+insert into weather.current (hr, dt, temp, pressure, humidity, dewpoint, rain)
+    Values (12, NULL, NULL, NULL, NULL, NULL, NULL);
+
+insert into weather.current (hr, dt, temp, pressure, humidity, dewpoint, rain)
+    Values (13, NULL, NULL, NULL, NULL, NULL, NULL);
+
+insert into weather.current (hr, dt, temp, pressure, humidity, dewpoint, rain)
+    Values (14, NULL, NULL, NULL, NULL, NULL, NULL);
+
+insert into weather.current (hr, dt, temp, pressure, humidity, dewpoint, rain)
+    Values (15, NULL, NULL, NULL, NULL, NULL, NULL);
+
+insert into weather.current (hr, dt, temp, pressure, humidity, dewpoint, rain)
+    Values (16, NULL, NULL, NULL, NULL, NULL, NULL);
+
+insert into weather.current (hr, dt, temp, pressure, humidity, dewpoint, rain)
+    Values (17, NULL, NULL, NULL, NULL, NULL, NULL);
+
+insert into weather.current (hr, dt, temp, pressure, humidity, dewpoint, rain)
+    Values (18, NULL, NULL, NULL, NULL, NULL, NULL);
+
+insert into weather.current (hr, dt, temp, pressure, humidity, dewpoint, rain)
+    Values (19, NULL, NULL, NULL, NULL, NULL, NULL);
+
+insert into weather.current (hr, dt, temp, pressure, humidity, dewpoint, rain)
+    Values (20, NULL, NULL, NULL, NULL, NULL, NULL);
+
+insert into weather.current (hr, dt, temp, pressure, humidity, dewpoint, rain)
+    Values (21, NULL, NULL, NULL, NULL, NULL, NULL);
+
+insert into weather.current (hr, dt, temp, pressure, humidity, dewpoint, rain)
+    Values (22, NULL, NULL, NULL, NULL, NULL, NULL);
+
+insert into weather.current (hr, dt, temp, pressure, humidity, dewpoint, rain)
+    Values (23, NULL, NULL, NULL, NULL, NULL, NULL);
+
+insert into weather.current (hr, dt, temp, pressure, humidity, dewpoint, rain)
+    Values (24, NULL, NULL, NULL, NULL, NULL, NULL);
+
+insert into weather.current (hr, dt, temp, pressure, humidity, dewpoint, rain)
+    Values (25, NULL, NULL, NULL, NULL, NULL, NULL);
+
+insert into weather.current (hr, dt, temp, pressure, humidity, dewpoint, rain)
+    Values (26, NULL, NULL, NULL, NULL, NULL, NULL);
+
+insert into weather.current (hr, dt, temp, pressure, humidity, dewpoint, rain)
+    Values (27, NULL, NULL, NULL, NULL, NULL, NULL);
+
+insert into weather.current (hr, dt, temp, pressure, humidity, dewpoint, rain)
+    Values (28, NULL, NULL, NULL, NULL, NULL, NULL);
+
+insert into weather.current (hr, dt, temp, pressure, humidity, dewpoint, rain)
+    Values (29, NULL, NULL, NULL, NULL, NULL, NULL);
+
+insert into weather.current (hr, dt, temp, pressure, humidity, dewpoint, rain)
+    Values (30, NULL, NULL, NULL, NULL, NULL, NULL);
+
+insert into weather.current (hr, dt, temp, pressure, humidity, dewpoint, rain)
+    Values (31, NULL, NULL, NULL, NULL, NULL, NULL);
+
+insert into weather.current (hr, dt, temp, pressure, humidity, dewpoint, rain)
+    Values (32, NULL, NULL, NULL, NULL, NULL, NULL);
+
+insert into weather.current (hr, dt, temp, pressure, humidity, dewpoint, rain)
+    Values (33, NULL, NULL, NULL, NULL, NULL, NULL);
+
+insert into weather.current (hr, dt, temp, pressure, humidity, dewpoint, rain)
+    Values (34, NULL, NULL, NULL, NULL, NULL, NULL);
+
+insert into weather.current (hr, dt, temp, pressure, humidity, dewpoint, rain)
+    Values (35, NULL, NULL, NULL, NULL, NULL, NULL);
+
+insert into weather.current (hr, dt, temp, pressure, humidity, dewpoint, rain)
+    Values (36, NULL, NULL, NULL, NULL, NULL, NULL);
+
+insert into weather.current (hr, dt, temp, pressure, humidity, dewpoint, rain)
+    Values (37, NULL, NULL, NULL, NULL, NULL, NULL);
+
+insert into weather.current (hr, dt, temp, pressure, humidity, dewpoint, rain)
+    Values (38, NULL, NULL, NULL, NULL, NULL, NULL);
+
+insert into weather.current (hr, dt, temp, pressure, humidity, dewpoint, rain)
+    Values (39, NULL, NULL, NULL, NULL, NULL, NULL);
+
+insert into weather.current (hr, dt, temp, pressure, humidity, dewpoint, rain)
+    Values (40, NULL, NULL, NULL, NULL, NULL, NULL);
+
+insert into weather.current (hr, dt, temp, pressure, humidity, dewpoint, rain)
+    Values (41, NULL, NULL, NULL, NULL, NULL, NULL);
+
+insert into weather.current (hr, dt, temp, pressure, humidity, dewpoint, rain)
+    Values (42, NULL, NULL, NULL, NULL, NULL, NULL);
+
+insert into weather.current (hr, dt, temp, pressure, humidity, dewpoint, rain)
+    Values (43, NULL, NULL, NULL, NULL, NULL, NULL);
+
+insert into weather.current (hr, dt, temp, pressure, humidity, dewpoint, rain)
+    Values (44, NULL, NULL, NULL, NULL, NULL, NULL);
+
+insert into weather.current (hr, dt, temp, pressure, humidity, dewpoint, rain)
+    Values (45, NULL, NULL, NULL, NULL, NULL, NULL);
+
+insert into weather.current (hr, dt, temp, pressure, humidity, dewpoint, rain)
+    Values (46, NULL, NULL, NULL, NULL, NULL, NULL);
+
+insert into weather.current (hr, dt, temp, pressure, humidity, dewpoint, rain)
+    Values (47, NULL, NULL, NULL, NULL, NULL, NULL);
+
---
+++ SCRAPER/scraper.py
@@ -0,0 +1,108 @@
+import os
+import requests
+import json
+import mysql.connector
+import datetime
+import time
+
+WEATHERUSER = os.environ("WEATHERUSER")
+DBPASS = os.environ("DBPASS")
+APIKEY_W = os.environ("APIKEY_W")
+
+
+class scraper():
+
+    def get_data(self, lat=53.3498, lon=-6.2603):
+        """Get the data from open weather maps api return as txt format"""
+        r = requests.get(
+            f"https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&appid=92fb08f48a98e0f39b990060352ffebe")
+        return r.text
+
+    def get_columns(self, current):
+        """get individual data columns from data"""
+        columns = {"dt": "NULL", "temp": "NULL", "pressure": "NULL", "humidity": "NULL", "dew_point": "NULL", "rain": 0}
+
+        try:
+            date = datetime.datetime.fromtimestamp(current["dt"])
+            columns["dt"] = date.strftime("%Y/%m/%d %H:%M:%S")
+        except Exception as e:
+            print("Error with date", e)
+
+        try:
+            C = current["temp"] - 273.15
+            columns["temp"] = C
+        except Exception as e:
+            print("Error with temp", e)
+
+        try:
+            columns["pressure"] = current["pressure"]
+        except Exception as e:
+            print("Error with pressure", e)
+
+        try:
+            columns["humidity"] = current["humidity"]
+        except Exception as e:
+            print("Error with humidity", e)
+
+        try:
+            columns["dew_point"] = current["dew_point"]
+        except Exception as e:
+            print("Error with dew_point", e)
+
+        try:
+            columns["rain"] = current["rain"]["1h"]
+        except Exception as e:
+            # expect that rain will not be there often due to nature of data
+            pass
+
+        return tuple(columns.values())
+
+    def process_data(self, data):
+        """Process the json data and place it into the database"""
+        parsed = json.loads(data)
+        current = parsed["current"]
+        future = parsed["hourly"]
+        current_res = self.get_columns(current)
+        print(current_res)
+        self.database_conn("current", -1, current_res)
+        for hour, data in enumerate(future):
+            current_res = self.get_columns(data)
+            print(current_res)
+            self.database_conn("current", hour, current_res)
+
+    def database_conn(self, table, hr, data):
+        """Create database connection and sql statement for placing data into database"""
+        # origionally from https://www.w3schools.com/python/python_mysql_insert.asp
+        mydb = mysql.connector.connect(
+            host="localhost",
+            user=WEATHERUSER,
+            password=DBPASS,
+            database="weather"
+        )
+        mycursor = mydb.cursor()
+        sql = f"UPDATE weather.{table} SET hr = {hr}, dt = %s, temp = %s, pressure = %s, humidity = %s, dewpoint = %s, rain = %s where hr = {hr}"
+        mycursor.execute(sql, data)
+        mydb.commit()
+
+    def run(self, mins=15):
+        """method to put run the entire scraper"""
+        attempts = 0
+        while True:
+            try:
+                data = self.get_data()
+                self.process_data(data)
+                attempts = 0
+                time.sleep(mins * 60)
+            except Exception as e:
+                print(e)
+                attempts += 1
+                if attempts > 5:
+                    print("Persistent Error quitting", datetime.now())
+                    break
+                time.sleep(1 * 60)
+                print("Error at", datetime.datetime.now())
+
+
+if __name__ == "__main__":
+    s = scraper()
+    s.run()
