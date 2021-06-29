8e30f8e9eff6b1ef74c8807ca2c3ce1e0edbc238
hassan <hassan.albujasim@ucdconnect.ie>
2021-06-29 20:07:14 +0100

    few adjusment. maybe we need a landing image

--- WEB_APP/doubledecker/views.py
+++ WEB_APP/doubledecker/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse # origionated from https://docs.djangoproject.com/en/3.2/intro/tutorial01/
+# origionated from https://docs.djangoproject.com/en/3.2/intro/tutorial01/
+from django.http import HttpResponse
 # Create your views here.
-def main(request): #  origionated from  https://docs.djangoproject.com/en/3.2/intro/tutorial01/
+
+
+def main(request):  # origionated from  https://docs.djangoproject.com/en/3.2/intro/tutorial01/
     return render(request, 'doubledecker/index.html')
