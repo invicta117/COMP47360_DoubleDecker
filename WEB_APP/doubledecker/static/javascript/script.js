fbaadf620f1eadd5c253606f2e7237663b6494f4
hassan <hassan.albujasim@ucdconnect.ie>
2021-06-29 20:17:29 +0100

    added user location button. maybe the map can just pan to user location automatically

--- WEB_APP/doubledecker/static/javascript/script.js
+++ WEB_APP/doubledecker/static/javascript/script.js
@@ -2,19 +2,13 @@
 // Initialize and add the map
 function initMap() {
     // The location of Uluru
-    const uluru = { lat: 53.3498, lng: -6.2603 };
+    const Dublin = { lat: 53.3498, lng: -6.2603 };
     // The map, centered at Uluru
     const map = new google.maps.Map(document.getElementById("map"), {
       zoom: 12,
-      center: uluru,
-});
-// The marker, positioned at Uluru
-const marker = new google.maps.Marker({
-  position: uluru,
-  map: map,
+      center: Dublin,
 });
 }
-
 // define a variable that get a button
 var x = document.getElementById('userLocation')
 function getLocation() {
