e917f3dc3c476a1b93182a29c6b54faa99ed74ff
hassan <hassan.albujasim@ucdconnect.ie>
2021-07-28 09:02:23 +0100

    remnoved A-B markers

--- WEB_APP/doubledecker/static/javascript/explore_script.js
+++ WEB_APP/doubledecker/static/javascript/explore_script.js
@@ -28,191 +28,227 @@ function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     zoom: 12,
     center: Dublin,
-    styles: [{
+    styles: [
+      {
         "elementType": "geometry",
-        "stylers": [{
-          "color": "#ebe3cd"
-        }]
+        "stylers": [
+          {
+            "color": "#ebe3cd"
+          }
+        ]
       },
       {
         "elementType": "labels.text.fill",
-        "stylers": [{
-          "color": "#523735"
-        }]
+        "stylers": [
+          {
+            "color": "#523735"
+          }
+        ]
       },
       {
         "elementType": "labels.text.stroke",
-        "stylers": [{
-          "color": "#f5f1e6"
-        }]
+        "stylers": [
+          {
+            "color": "#f5f1e6"
+          }
+        ]
       },
       {
         "featureType": "administrative",
         "elementType": "geometry.stroke",
-        "stylers": [{
-          "color": "#c9b2a6"
-        }]
+        "stylers": [
+          {
+            "color": "#c9b2a6"
+          }
+        ]
       },
       {
         "featureType": "administrative.land_parcel",
         "elementType": "geometry.stroke",
-        "stylers": [{
-          "color": "#dcd2be"
-        }]
+        "stylers": [
+          {
+            "color": "#dcd2be"
+          }
+        ]
       },
       {
         "featureType": "administrative.land_parcel",
         "elementType": "labels.text.fill",
-        "stylers": [{
-          "color": "#ae9e90"
-        }]
+        "stylers": [
+          {
+            "color": "#ae9e90"
+          }
+        ]
       },
       {
         "featureType": "landscape.natural",
         "elementType": "geometry",
-        "stylers": [{
-          "color": "#dfd2ae"
-        }]
+        "stylers": [
+          {
+            "color": "#dfd2ae"
+          }
+        ]
       },
       {
         "featureType": "poi",
         "elementType": "geometry",
-        "stylers": [{
-          "color": "#dfd2ae"
-        }]
+        "stylers": [
+          {
+            "color": "#dfd2ae"
+          }
+        ]
       },
       {
         "featureType": "poi",
         "elementType": "labels.text.fill",
-        "stylers": [{
-          "color": "#93817c"
-        }]
+        "stylers": [
+          {
+            "color": "#93817c"
+          }
+        ]
       },
       {
         "featureType": "poi.park",
         "elementType": "geometry.fill",
-        "stylers": [{
-          "color": "#a5b076"
-        }]
-      },
-      {
-        "featureType": "poi.park",
-        "elementType": "labels.icon",
-        "stylers": [{
-          "weight": 2
-        }]
-      },
-      {
-        "featureType": "poi.park",
-        "elementType": "labels.text",
-        "stylers": [{
-          "weight": 3.5
-        }]
+        "stylers": [
+          {
+            "color": "#a5b076"
+          }
+        ]
       },
       {
         "featureType": "poi.park",
         "elementType": "labels.text.fill",
-        "stylers": [{
-          "color": "#447530"
-        }]
+        "stylers": [
+          {
+            "color": "#447530"
+          }
+        ]
       },
       {
         "featureType": "road",
         "elementType": "geometry",
-        "stylers": [{
-          "color": "#f5f1e6"
-        }]
-      },
-      {
-        "featureType": "road",
-        "elementType": "labels.text",
-        "stylers": [{
-          "visibility": "off"
-        }]
+        "stylers": [
+          {
+            "color": "#f5f1e6"
+          }
+        ]
       },
       {
         "featureType": "road.arterial",
         "elementType": "geometry",
-        "stylers": [{
-          "color": "#fdfcf8"
-        }]
-      },
-      {
-        "featureType": "road.arterial",
-        "elementType": "labels.icon",
-        "stylers": [{
-          "visibility": "off"
-        }]
+        "stylers": [
+          {
+            "color": "#fdfcf8"
+          }
+        ]
       },
       {
         "featureType": "road.highway",
         "elementType": "geometry",
-        "stylers": [{
-          "color": "#f8c967"
-        }]
+        "stylers": [
+          {
+            "color": "#f8c967"
+          }
+        ]
+      },
+      {
+        "featureType": "road.highway",
+        "elementType": "geometry.stroke",
+        "stylers": [
+          {
+            "color": "#e9bc62"
+          }
+        ]
       },
       {
         "featureType": "road.highway.controlled_access",
         "elementType": "geometry",
-        "stylers": [{
-          "color": "#e98d58"
-        }]
+        "stylers": [
+          {
+            "color": "#e98d58"
+          }
+        ]
       },
       {
         "featureType": "road.highway.controlled_access",
         "elementType": "geometry.stroke",
-        "stylers": [{
-          "color": "#db8555"
-        }]
+        "stylers": [
+          {
+            "color": "#db8555"
+          }
+        ]
       },
       {
         "featureType": "road.local",
         "elementType": "labels.text.fill",
-        "stylers": [{
-          "color": "#806b63"
-        }]
+        "stylers": [
+          {
+            "color": "#806b63"
+          }
+        ]
       },
       {
         "featureType": "transit.line",
         "elementType": "geometry",
-        "stylers": [{
-          "color": "#dfd2ae"
-        }]
+        "stylers": [
+          {
+            "color": "#dfd2ae"
+          }
+        ]
       },
       {
         "featureType": "transit.line",
         "elementType": "labels.text.fill",
-        "stylers": [{
-          "color": "#8f7d77"
-        }]
+        "stylers": [
+          {
+            "color": "#8f7d77"
+          }
+        ]
       },
       {
         "featureType": "transit.line",
         "elementType": "labels.text.stroke",
-        "stylers": [{
-          "color": "#ebe3cd"
-        }]
+        "stylers": [
+          {
+            "color": "#ebe3cd"
+          }
+        ]
       },
       {
         "featureType": "transit.station",
         "elementType": "geometry",
-        "stylers": [{
-          "color": "#dfd2ae"
-        }]
+        "stylers": [
+          {
+            "color": "#dfd2ae"
+          }
+        ]
+      },
+      {
+        "featureType": "transit.station.bus",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
       },
       {
         "featureType": "water",
         "elementType": "geometry.fill",
-        "stylers": [{
-          "color": "#b9d3c2"
-        }]
+        "stylers": [
+          {
+            "color": "#b9d3c2"
+          }
+        ]
       },
       {
         "featureType": "water",
         "elementType": "labels.text.fill",
-        "stylers": [{
-          "color": "#92998d"
-        }]
+        "stylers": [
+          {
+            "color": "#92998d"
+          }
+        ]
       }
     ]
   });
@@ -250,6 +286,7 @@ function initMap() {
 
           directionsDisplay = new google.maps.DirectionsRenderer({
             map: map,
+            suppressMarkers: true
           });
           calculateAndDisplayRoute(
             directionsService,
@@ -294,7 +331,11 @@ function calculateAndDisplayRoute(
   directionsService.route({
       origin: startPoint,
       destination: endPoint,
-      travelMode: "TRANSIT",
+      travelMode: 'TRANSIT',
+      transitOptions: {
+        modes: ['BUS'],
+        routingPreference: 'FEWER_TRANSFERS'
+      },
     },
     function (response, status) {
       if (status === "OK") {
@@ -303,7 +344,7 @@ function calculateAndDisplayRoute(
         bounds.union(response.routes[0].bounds);
         map.fitBounds(bounds);
       } else {
-        window.alert("Impossible d afficher la route " + status);
+        window.alert("There is no route in this name working at this time " + status);
       }
     }
   );
