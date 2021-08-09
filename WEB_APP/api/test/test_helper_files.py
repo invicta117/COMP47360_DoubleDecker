bd651709f933d53a026efd8dc69013a7cf3d0886
KC <cooneykillian@gmail.com>
2021-08-09 16:09:00 +0100

    adding more tests and fixing some bugs that showed up from testing

---
+++ WEB_APP/api/test/test_helper_files.py
@@ -0,0 +1,20 @@
+# the following is based on https://www.youtube.com/watch?v=0MrgsYswT1c&list=PLbpAWbHbi5rMF2j5n6imm0enrSD9eQUaM&index=2
+from django.test import TestCase,RequestFactory
+from rest_framework.response import Response
+
+# Create your test here.
+from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework.test import APIRequestFactory
+from rest_framework.test import APIClient
+from django.urls import resolve, reverse
+from api.views import apiOverview,ShowAllRoutes,ShowAllWeather,ShowAllRouteStops,ShowCurrentWeather, ShowAllRouteLine,ShowRouteLine,routeStation
+from api.routeStations import explore
+# we can import the module we wanna test
+
+
+class TestViews(TestCase):
+    def test_routeStations(self):
+        self.explore = explore("Wednesday", "7a")
+        self.assertEquals(type(self.explore), list)
+        self.assertNotEqual(self.explore, [])
--- WEB_APP/doubledecker/models.py
+++ WEB_APP/doubledecker/models.py
@@ -33,3 +33,232 @@ class Weather(models.Model):
 
     class Meta:
         db_table = 'weather'
+
+
+class AuthGroup(models.Model):
+    name = models.CharField(unique=True, max_length=150)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group'
+
+
+class AuthGroupPermissions(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+    permission = models.ForeignKey('AuthPermission', models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group_permissions'
+        unique_together = (('group', 'permission'),)
+
+
+class AuthPermission(models.Model):
+    name = models.CharField(max_length=255)
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING)
+    codename = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_permission'
+        unique_together = (('content_type', 'codename'),)
+
+
+class AuthUser(models.Model):
+    password = models.CharField(max_length=128)
+    last_login = models.DateTimeField(blank=True, null=True)
+    is_superuser = models.IntegerField()
+    username = models.CharField(unique=True, max_length=150)
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    email = models.CharField(max_length=254)
+    is_staff = models.IntegerField()
+    is_active = models.IntegerField()
+    date_joined = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user'
+
+
+class AuthUserGroups(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_groups'
+        unique_together = (('user', 'group'),)
+
+
+class AuthUserUserPermissions(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    permission = models.ForeignKey(AuthPermission, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_user_permissions'
+        unique_together = (('user', 'permission'),)
+
+
+class Calendar(models.Model):
+    service_id = models.CharField(primary_key=True, max_length=200)
+    monday = models.IntegerField(blank=True, null=True)
+    tuesday = models.IntegerField(blank=True, null=True)
+    wednesday = models.IntegerField(blank=True, null=True)
+    thursday = models.IntegerField(blank=True, null=True)
+    friday = models.IntegerField(blank=True, null=True)
+    saturday = models.IntegerField(blank=True, null=True)
+    sunday = models.IntegerField(blank=True, null=True)
+    start_date = models.CharField(max_length=10, blank=True, null=True)
+    end_date = models.CharField(max_length=10, blank=True, null=True)
+
+    class Meta:
+        db_table = 'calendar'
+
+
+class CalendarDates(models.Model):
+    service = models.OneToOneField(
+    Calendar, models.DO_NOTHING, primary_key=True)
+    date = models.CharField(max_length=10)
+    exception_type = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        db_table = 'calendar_dates'
+        unique_together = (('service', 'date'),)
+
+
+class DjangoAdminLog(models.Model):
+    action_time = models.DateTimeField()
+    object_id = models.TextField(blank=True, null=True)
+    object_repr = models.CharField(max_length=200)
+    action_flag = models.PositiveSmallIntegerField()
+    change_message = models.TextField()
+    content_type = models.ForeignKey(
+        'DjangoContentType', models.DO_NOTHING, blank=True, null=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'django_admin_log'
+
+
+class DjangoContentType(models.Model):
+    app_label = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'django_content_type'
+        unique_together = (('app_label', 'model'),)
+
+
+class DjangoMigrations(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    app = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    applied = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_migrations'
+
+
+class DjangoSession(models.Model):
+    session_key = models.CharField(primary_key=True, max_length=40)
+    session_data = models.TextField()
+    expire_date = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_session'
+
+
+class Routes(models.Model):
+    route_id = models.CharField(primary_key=True, max_length=30)
+    agency_id = models.CharField(max_length=30, blank=True, null=True)
+    route_short_name = models.CharField(max_length=10, blank=True, null=True)
+    route_long_name = models.CharField(max_length=255, blank=True, null=True)
+    route_type = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        db_table = 'routes'
+
+
+class Shapes(models.Model):
+    shape_id = models.CharField(primary_key=True, max_length=30)
+    shape_pt_lat = models.FloatField(blank=True, null=True)
+    shape_pt_lon = models.FloatField(blank=True, null=True)
+    shape_pt_sequence = models.SmallIntegerField()
+    shape_dist_traveled = models.FloatField(blank=True, null=True)
+
+    class Meta:
+        db_table = 'shapes'
+        unique_together = (('shape_id', 'shape_pt_sequence'),)
+
+
+class StopTimes(models.Model):
+    trip = models.OneToOneField('Trips', models.DO_NOTHING, primary_key=True)
+    arrival_time = models.TimeField()
+    departure_time = models.TimeField(blank=True, null=True)
+    stop = models.ForeignKey('Stops', models.DO_NOTHING)
+    stop_sequence = models.IntegerField(blank=True, null=True)
+    stop_headsign = models.CharField(max_length=200, blank=True, null=True)
+    pickup_type = models.IntegerField(blank=True, null=True)
+    drop_off_type = models.IntegerField(blank=True, null=True)
+    shape_dist_traveled = models.FloatField(blank=True, null=True)
+
+    class Meta:
+        db_table = 'stop_times'
+        unique_together = (('trip', 'arrival_time', 'stop'),)
+
+
+class Stops(models.Model):
+    stop_id = models.CharField(primary_key=True, max_length=30)
+    stop_name = models.CharField(max_length=200)
+    stop_lat = models.FloatField(blank=True, null=True)
+    stop_lon = models.FloatField(blank=True, null=True)
+
+    class Meta:
+        db_table = 'stops'
+        unique_together = (('stop_id', 'stop_name'),)
+
+
+class Trips(models.Model):
+    route = models.ForeignKey(Routes, models.DO_NOTHING, blank=True, null=True)
+    service = models.ForeignKey(
+        Calendar, models.DO_NOTHING, blank=True, null=True)
+    trip_id = models.CharField(primary_key=True, max_length=60)
+    shape = models.ForeignKey(Shapes, models.DO_NOTHING, blank=True, null=True)
+    trip_headsign = models.CharField(max_length=200, blank=True, null=True)
+    direction_id = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        db_table = 'trips'
+
+
+class RouteStops(models.Model):
+    stop_name = models.CharField(primary_key=True, max_length=255)
+    stop_lat = models.FloatField(blank=True, null=True)
+    stop_lon = models.FloatField(blank=True, null=True)
+    route_short_name = models.CharField(max_length=225)
+
+    class Meta:
+        db_table = 'route_stops'
+        unique_together = (('stop_name', 'route_short_name'),)
+
+
+class SeqRoutes(models.Model):
+    route_id = models.CharField(max_length=60, primary_key=True)
+    route_short_name = models.CharField(max_length=60, blank=True)
+    shape_pt_lat = models.FloatField(blank=True, null=True)
+    shape_pt_lon = models.FloatField(blank=True, null=True)
+    shape_pt_sequence = models.SmallIntegerField()
+
+    class Meta:
+        db_table = 'seq_routes'
+        unique_together = (('route_id', 'route_short_name'),)
+
