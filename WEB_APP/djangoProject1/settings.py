df2362b0efee279285db64f8cbf2b99d49b48854
KC <cooneykillian@gmail.com>
2021-08-19 14:28:10 +0100

    using os environment for passwords again

--- WEB_APP/djangoProject1/settings.py
+++ WEB_APP/djangoProject1/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'gtfs',
         'USER': 'student',
-        'PASSWORD': "123",
+        'PASSWORD': os.environ["DBPASS"],
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
